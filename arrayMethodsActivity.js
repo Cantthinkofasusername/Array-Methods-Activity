24085163c5aa319c80b1dc29a9400b447f8d8e5f
Your Name <shado@LAPTOP-U7GG9IKM>
2024-10-16 04:24:24 +0800

    initial commit

---
+++ arrayMethodsActivity.js
@@ -0,0 +1,30 @@
+let arr1 = ["Cecile", "Lone"];
+let arr2 = ["Emil", "Tobias", "Linus"];
+
+console.log(arr1.concat(arr2));
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.push("Kiwi");
+console.log(fruits);
+
+array1 = [1, 2, 3];
+
+array1.unshift(4, 5);
+console.log(array1);
+
+fruits.pop();
+console.log(fruits);
+
+array2 = [1, 2, 3];
+
+array2.shift([0]);
+console.log(array2);
+
+console.log(fruits.sort());
+
+console.log(fruits.slice(0, 2));
+
+months = ["January", "February", "March", "April", "May", "June"];
+months.splice(3, 2, "July", "August");
+console.log(months);
